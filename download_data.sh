02a1664d1cc93c411a4fe199a70cbb216902d6b7
Daniel Morales <79028039+dmora127@users.noreply.github.com>
2025-10-14 11:28:37 -0500

    Update submission scripts and download_data.sh for new input paths and improved logging

--- download_data.sh
+++ download_data.sh
@@ -8,15 +8,15 @@ mkdir -p ~/tutorial-minimap2/{inputs, outputs}
 #osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/minimap2_08OCT2025_v1.sif /ospool/guest-ap/data/jovyan/tutorial-minimap2/software/
 #osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/humpback_whale_genome_09OCT2025_v1.fasta.mmi /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs/
 
-osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/minimap2_08OCT2025_v1.sif /ospool/guest-ap/data/jovyan/tutorial-minimap2/software/
-echo "A pre-built container of Minimap2 is at /ospool/guest-ap/data/jovyan/tutorial-minimap2/software/minimap2_08OCT2025_v1.sif"
+osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/minimap2_08OCT2025_v1.sif /ospool/guest-ap/data/jovyan/tutorial-minimap2/software/ > /dev/null
+echo "A pre-built container of Minimap2 is at /ospool/guest-ap/data/jovyan/tutorial-minimap2/software/minimap2_08OCT2025_v1.sif ✅"
 echo ""
 
-osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/humpback_whale_genome_09OCT2025_v1.fasta.mmi /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs/
-echo "A pre-indexed reference genome is at /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs/humpback_whale_genome_09OCT2025_v1.fasta.mmi"
+osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/humpback_whale_ref_genome.fasta.mmi /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs/ > /dev/null
+echo "A pre-indexed reference genome is at /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs/humpback_whale_ref_genome.fasta.mmi ✅"
 echo ""
 
-osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/h_whale_sd_0001_sub.fastq.tar.gz?pack=tar.gz ./inputs/
-mv inputs/h_whale_sd_0001_sub.fastq inputs/m_novaeangliae.fastq
-echo "A copy of the WGS reads is at ./inputs/h_whale_sd_0001_sub.fastq"
-echo "Data download complete."
+osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/h_whale_sd_0001_sub.fastq.tar.gz?pack=tar.gz ./inputs/ > /dev/null
+mv inputs/h_whale_sd_0001_sub.fastq inputs/humpback_whale_reads.fastq
+echo "A copy of the WGS reads is at ./inputs/h_whale_sd_0001_sub.fastq ✅"
+echo "Data download complete. ✅"
