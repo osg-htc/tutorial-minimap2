6a50d9424a37cc73cf75ef831c01d3b7ee2caec3
Daniel Morales <79028039+dmora127@users.noreply.github.com>
2025-10-14 11:56:35 -0500

    Add should_transfer_files flag to submission scripts and enhance download_data.sh logging

--- download_data.sh
+++ download_data.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 # Downloads necessary data files for the Minimap2 tutorial from OSG Public Storage to the local environment.
-mkdir -p /ospool/guest-ap/data/jovyan/tutorial-minimap2/{software,inputs}
-mkdir -p ~/tutorial-minimap2/{inputs, outputs}
+mkdir -p /ospool/guest-ap/data/jovyan/tutorial-minimap2/software
+mkdir -p /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs
+mkdir -p ~/tutorial-minimap2/inputs
+mkdir -p ~/tutorial-minimap2/outputs
+
+echo "Created necessary directory structure ✅"
 
 #osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/humpback_whale_genome_09OCT2025_v1.fasta ./inputs/
 #osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/h_whale_sd_0001_sub.fastq.gz ./inputs/
 #osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/minimap2_08OCT2025_v1.sif /ospool/guest-ap/data/jovyan/tutorial-minimap2/software/
 #osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/humpback_whale_genome_09OCT2025_v1.fasta.mmi /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs/
 
+echo "⏳ Starting data download from OSG Public OSDF Directory...⏳ ⬇️"
+
 osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/minimap2_08OCT2025_v1.sif /ospool/guest-ap/data/jovyan/tutorial-minimap2/software/ > /dev/null
 echo "A pre-built container of Minimap2 is at /ospool/guest-ap/data/jovyan/tutorial-minimap2/software/minimap2_08OCT2025_v1.sif ✅"
 echo ""
 
-osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/humpback_whale_ref_genome.fasta.mmi /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs/ > /dev/null
+osdf object get /ospool/uc-shared/public/osg-training/tutorial-minimap2/inputs/humpback_whale_ref_genome.fasta.mmi /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs/ > /dev/null
 echo "A pre-indexed reference genome is at /ospool/guest-ap/data/jovyan/tutorial-minimap2/inputs/humpback_whale_ref_genome.fasta.mmi ✅"
 echo ""
 
